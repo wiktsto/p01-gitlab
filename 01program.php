4cc8b597174c2905eb3c54a1c73fb0b639dfeab2
wiktsto <49189012+wiktsto@users.noreply.github.com>
2019-05-21 11:07:37 +0200

    Piaty commit

--- 01program.php
+++ 01program.php
@@ -4,5 +4,6 @@ echo ("<br> Jestem programem 01");
 echo ("<br> Test 1111111");
 echo ("<br> TEST 222222");
 echo ("<br>Dalsza czesc programu");
+echo ("<br> Dalsza czesc programu 22222 z GitHuba");
 ?>
 
